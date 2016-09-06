ed233389c46bd4ec6729a8180b07b1ffdd930a5a
Ankit Sinha <ankit@tagbin.in>
2016-09-06 14:51:45 +0530

    Started Dev on Converse

--- converse.js
+++ converse.js
@@ -19,17 +19,17 @@ if (typeof define !== 'undefined') {
                                 // file src/locales.js to include only those
                                 // translations that you care about.
 
-        "converse-chatview",    // Renders standalone chat boxes for single user chat
+        //"converse-chatview",    // Renders standalone chat boxes for single user chat
         "converse-controlbox",  // The control box
         "converse-mam",         // XEP-0313 Message Archive Management
         "converse-muc",         // XEP-0045 Multi-user chat
         "converse-vcard",       // XEP-0054 VCard-temp
         "converse-otr",         // Off-the-record encryption for one-on-one messages
-        "converse-register",    // XEP-0077 In-band registration
+        //"converse-register",    // XEP-0077 In-band registration
         "converse-ping",        // XEP-0199 XMPP Ping
         "converse-notification",// HTML5 Notifications
-        "converse-minimize",    // Allows chat boxes to be minimized
-        "converse-dragresize",  // Allows chat boxes to be resized by dragging them
+        //"converse-minimize",    // Allows chat boxes to be minimized
+        //"converse-dragresize",  // Allows chat boxes to be resized by dragging them
         "converse-headline",    // Support for headline messages
         /* END: Removable components */
 
